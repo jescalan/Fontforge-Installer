3f694fec8a4766c189661f4628daa6f94813cab2
Jeff Escalante <hello@jenius.me>
2012-03-07 10:01:53 -0500

    header

--- FontForge Installer/fontforgeAppDelegate.h
+++ FontForge Installer/fontforgeAppDelegate.h
@@ -11,5 +11,10 @@
 @interface fontforgeAppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSButton *installButton;
+@property (assign) IBOutlet NSTextField *installStatus;
+@property (assign) IBOutlet NSProgressIndicator *spinner;
+
+- (IBAction)installFontforge:(id)sender;
 
 @end
